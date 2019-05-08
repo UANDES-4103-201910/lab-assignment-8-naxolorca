c2b758ed32e9899151ed8fadbb21673f01af11fd
naxolorca <filorca@miuandes.cl>
2019-05-08 11:33:55 -0400

    hola

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,11 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	$('.key').click(function(){
+	var show = "";
+	show = $('#text').text() + $(this).text();
+
+	console.log($(this).text())
+	$('#text').text(show);
+	});
 });
