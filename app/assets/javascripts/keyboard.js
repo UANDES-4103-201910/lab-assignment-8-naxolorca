b6892bb39de42692a3e96944172944ecf4ad5c72
naxolorca <filorca@miuandes.cl>
2019-05-09 23:48:02 -0400

    terminado

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,11 +1,28 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	var id = 1
 	$('.key').click(function(){
-	var show = "";
-	show = $('#text').text() + $(this).text();
+		var show = "";
+		var key = $(this).text();
+	if ($(this).text() == 'Enter'){
+		id += 1
+		key = ''
+	}
+	if ($(this).text() == 'Bksp'){
+		if($('#id'+id).text() == ''){
+			id-=1
+		}
+
+		show = $('#id'+id).text().slice(0,-1)
+	}
+	else{
+		show = $('#id'+id).text() + key;
+	}
 
 	console.log($(this).text())
-	$('#text').text(show);
+
+	$('#id'+id).text(show);
+
 	});
 });
